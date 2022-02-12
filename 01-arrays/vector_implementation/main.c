82b92fa63d57d90553baaf4b30698ecf36103a66
Blue Eu <eu.blue@pm.me>
2022-02-12 14:21:22 +0900

    vector implementation finished

--- 01-arrays/vector_implementation/main.c
+++ 01-arrays/vector_implementation/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define INF 0x7FFFFFFF
 #define TRUE 1
@@ -13,48 +14,101 @@ typedef struct {
   int size;
 } Vector;
 
+// private function
+static void resize(Vector *, int);
+
 // public function
 void init(Vector *);
+void destroy(Vector *);
+
+void push_back(Vector *, int);
+int pop_back(Vector *);
+void insert(Vector *, int, int);
+int delete(Vector *, int);
+void _remove(Vector *, int);
+void prepend(Vector *, int);
+int at(Vector *, int);
+int find(Vector *, int);
+
 int size(Vector *);
 int capacity(Vector *);
 _Bool is_empty(Vector *);
 void print(Vector *);
 
-void push(Vector *, int);
-int pop(Vector *);
-int at(Vector *, int);
-
-// private function
-static void resize(Vector *, int);
-
 int main(void) {
-  Vector vec;
+  unsigned seed = time(0);
+  srand(seed);
 
+  Vector vec;
   init(&vec);
-  printf("number of items: %d\n", size(&vec));
-  printf("number of items it can hold: %d\n", capacity(&vec));
-  printf("is it empty?: %d\n", is_empty(&vec));
-  printf("size at index 0: %d\n", at(&vec, 0));
-  printf("size at index 1: %d\n", at(&vec, 1));
-  printf("size at index 20: %d\n\n", at(&vec, 20));
 
   for(int i=0; i<25; ++i) {
-    push(&vec, i);
-    printf("push %d\n", i);
+    int data = rand() % 10;
+    push_back(&vec, data);
+    printf("push_back %d\n", data);
     print(&vec);
   }
 
-  for(int i=0; i<30; ++i) {
-    printf("popped %d\n", pop(&vec));
+  printf("is 5 in vector? index: %d\n", find(&vec, 5));
+  printf("is 177 in vector? index: %d\n", find(&vec, 177));
+
+  for(int i=0; i<5; ++i) {
+    printf("popped %d\n", pop_back(&vec));
     print(&vec);
   }
 
-  push(&vec, 17);
-  printf("push %d\n", 17);
+  for(int i=0; i<10; ++i) {
+    int index = rand() % capacity(&vec);
+    int data = rand() % 10;
+    printf("insert %d at index %d\n", data, index);
+    insert(&vec, index, data);
+    print(&vec);
+  }
+
+  printf("prepend 10\n");
+  printf("push_back 11\n");
+  printf("insert 12 at 2\n");
+  prepend(&vec, 10);
+  push_back(&vec, 11);
+  insert(&vec, 2, 12);
   print(&vec);
 
-  free(vec.arr);
+  for(int i=0; i<5; ++i) {
+    int idx = rand() % 10;
+    printf("delete a value at index: %d -  %d\n", idx, delete(&vec, idx));
+    print(&vec);
+  }
+
+  for(int i=0; i<5; ++i) {
+    int data = rand() % 10;
+    printf("remove %d\n", data);
+    _remove(&vec, data);
+    print(&vec);
+  }
+
+  destroy(&vec);
+
   return 0;
+}
+
+/******************************************************* 
+ * resize - private function
+ *
+ * 1) Double the size if vector is full. 
+ * 2) halve the size if the size is 1/4 of capacity. 
+ ******************************************************/
+static void resize(Vector *vec, int capacity) {
+  vec->capacity = capacity;
+  int *temp = (int *)(malloc(vec->capacity * sizeof(int)));
+
+  // copy over size
+  for(int i=0; i<vec->size; ++i) {
+    temp[i] = (vec->arr)[i];
+  }
+
+  free(vec->arr);
+
+  vec->arr = temp;
 }
 
 void init(Vector *vec) {
@@ -65,6 +119,165 @@ void init(Vector *vec) {
   for(int i=0; i<DEFAULT_SIZE; ++i) {
     (vec->arr)[i] = INF;
   }
+}
+
+void destroy(Vector *vec) {
+  free(vec->arr);
+  vec->arr = NULL;
+  vec->size = 0;
+  vec->capacity = 0;
+}
+
+void push_back(Vector *vec, int size) {
+  // double the size if vector is full
+  if(vec->size == vec->capacity) {
+    resize(vec, vec->capacity << 1);
+  }
+
+  (vec->arr)[vec->size] = size;
+  ++(vec->size);
+}
+
+int pop_back(Vector *vec) {
+  if(is_empty(vec)) {
+    printf(".... vector is empty\n");
+    return -1;
+  }
+
+  // if size is 1/4 of capacity, halve the capacity
+  int quart = vec->capacity / 4;
+  if(vec->capacity > DEFAULT_SIZE && vec->size <= quart) {
+    resize(vec, vec->capacity >> 1);
+  }
+
+  --(vec->size);
+  int data = (vec->arr)[vec->size];
+  (vec->arr)[vec->size] = INF;
+
+  return data;
+}
+
+/******************************************************* 
+ * index = zero-based (0, 1, 2, ...)
+ ******************************************************/
+void insert(Vector *vec, int idx, int data) {
+  // double the size if vector is full
+  if(vec->size == vec->capacity) {
+    resize(vec, vec->capacity << 1);
+  }
+
+  if(idx == 0) {
+    prepend(vec, data);
+  } else if (idx >= vec->size) {
+    push_back(vec, data);
+  } else {
+    int *temp = (int *)(malloc(vec->capacity * sizeof(int)));
+    for(int i=0; i<idx; ++i) {
+      temp[i] = (vec->arr)[i];
+    }
+
+    temp[idx] = data;
+
+    for(int i=idx; i<vec->size; ++i) {
+      temp[i+1] = (vec->arr)[i];
+    }
+
+    ++(vec->size);
+    free(vec->arr);
+    vec->arr = temp;
+  }
+}
+
+/******************************************************* 
+ * index = zero-based (0, 1, 2, ...)
+ ******************************************************/
+int delete(Vector *vec, int idx) {
+  if(idx >= vec->size) {
+    return pop_back(vec);
+  }
+
+  int *temp = (int *)(malloc(vec->capacity * sizeof(int)));
+  int data = (vec->arr)[idx];
+
+  for(int i=0; i<idx; ++i) {
+    temp[i] = (vec->arr)[i];
+  }
+
+  for(int i=idx; i<vec->size; ++i) {
+    temp[i] = (vec->arr)[i+1];
+  }
+
+  --(vec->size);
+  free(vec->arr);
+  vec->arr = temp;
+
+  return data;
+}
+
+void _remove(Vector *vec, int data) {
+  if(is_empty(vec)) return;
+
+  int p = 0;
+  int *temp = (int *)(malloc(vec->capacity * sizeof(int)));
+  for(int i=0; i<vec->size; ++i) {
+    if((vec->arr)[i] != data) {
+      temp[p] = (vec->arr)[i]; 
+      ++p;
+    }
+  }
+
+  vec->size = p;
+  free(vec->arr);
+  vec->arr = temp;
+
+  // if size is 1/4 of capacity, halve the capacity
+  int quart = vec->capacity / 4;
+  if(vec->capacity > DEFAULT_SIZE && vec->size <= quart) {
+    resize(vec, vec->capacity >> 1);
+  }
+}
+
+void prepend(Vector *vec, int data) {
+  // double the size if vector is full
+  if(vec->size == vec->capacity) {
+    resize(vec, vec->capacity << 1);
+  }
+
+  int *temp = (int *)(malloc(vec->capacity * sizeof(int)));
+  temp[0] = data;
+  for(int i=0; i<vec->size; ++i) {
+    temp[i+1] = (vec->arr)[i];
+  }
+
+  ++(vec->size);
+  free(vec->arr);
+  vec->arr = temp;
+}
+
+/******************************************************* 
+ * index = zero-based (0, 1, 2, ...)
+ ******************************************************/
+int at(Vector *vec, int idx) {
+  // out of bound
+  if(idx >= vec->capacity || (vec->arr)[idx] == INF) {
+    printf("....can't access index %d - overflow\n", idx);
+    return -1;
+  }
+
+  return (vec->arr)[idx];
+}
+
+int find(Vector *vec, int data) {
+  int idx = -1;
+
+  for(int i=0; i<vec->size; ++i) {
+    if((vec->arr)[i] == data) {
+      idx = i;
+      break;
+    }
+  }
+
+  return idx;
 }
 
 int size(Vector *vec) {
@@ -81,60 +294,12 @@ _Bool is_empty(Vector *vec) {
 
 void print(Vector *vec) {
   for(int i=0; i<vec->size; ++i) {
-    printf("%d ", (vec->arr)[i]);   
+    printf("%3d ", (vec->arr)[i]);   
+
+    if((i+1)%10 == 0)  {
+      printf("\n");
+    }
   }
-  printf(" --- size / capacity (%d / %d)\n", vec->size, vec->capacity);
+  printf("\n ---- size / capacity (%d / %d)\n\n", vec->size, vec->capacity);
 }
 
-int at(Vector *vec, int idx) {
-  // out of bound
-  if(idx >= vec->capacity || (vec->arr)[idx] == INF) {
-    printf("....can't access index %d - overflow\n", idx);
-    return -1;
-  }
-
-  return (vec->arr)[idx];
-}
-
-void push(Vector *vec, int size) {
-  // double the size if vector is full
-  if(vec->size == vec->capacity) {
-    resize(vec, vec->capacity << 1);
-  }
-
-  (vec->arr)[vec->size] = size;
-  ++(vec->size);
-}
-
-int pop(Vector *vec) {
-  if(is_empty(vec)) {
-    printf(".... vector is empty\n");
-    return -1;
-  }
-
-  // if size is 1/4 of capacity, resize to half
-  int quart = vec->capacity / 4;
-  if(vec->capacity > DEFAULT_SIZE && vec->size <= quart) {
-    resize(vec, vec->capacity >> 1);
-  }
-
-  --(vec->size);
-  int data = (vec->arr)[vec->size];
-  (vec->arr)[vec->size] = INF;
-
-  return data;
-}
-
-static void resize(Vector *vec, int capacity) {
-  vec->capacity = capacity;
-  int *temp = (int *)(malloc(vec->capacity * sizeof(int)));
-
-  // copy over size
-  for(int i=0; i<vec->size; ++i) {
-    temp[i] = (vec->arr)[i];
-  }
-
-  free(vec->arr);
-
-  vec->arr = temp;
-}
--- README.md
+++ README.md
@@ -39,26 +39,26 @@ I added and removed some parts to make it more suitable for myself to study (e.g
 ## Data Structures
 - ### [Arrays](https://www.geeksforgeeks.org/array-data-structure/)
     - [x] [Introduction](https://www.geeksforgeeks.org/introduction-to-arrays/)
-    - [ ] Implement a vector (mutable array with automatic resizing):
+    - [x] Implement a vector (mutable array with automatic resizing):
       - [x] New raw data array with allocated memory (default = 16, or power of 2 - 16, 32, 64, ...)
       - [x] size() - number of items
       - [x] capacity() - number of items it can hold
       - [x] is_empty()
       - [x] at(index) - returns item at given index, blows up if index out of bounds
       - [x] push(item)
-      - [ ] insert(index, item) - inserts item at index, shifts that index's value and trailing elements to the right
-      - [ ] prepend(item) - can use insert above at index 0
+      - [x] insert(index, item) - inserts item at index, shifts that index's value and trailing elements to the right
+      - [x] prepend(item) - inserts item at index 0
       - [x] pop() - remove from end, return value
-      - [ ] delete(index) - delete item at index, shifting all trailing elements left
-      - [ ] remove(item) - looks for value and removes index holding it (even if in multiple places)
-      - [ ] find(item) - looks for value and returns first index with that value, -1 if not found
+      - [x] delete(index) - delete item at index, shifting all trailing elements left
+      - [x] remove(item) - looks for value and removes index holding it (even if in multiple places)
+      - [x] find(item) - looks for value and returns first index with that value, -1 if not found
       - [x] resize(new_capacity) // private function
         - [x] when you reach capacity, resize to double the size
         - [x] when popping an item, if size is 1/4 of capacity, resize to half
-    - [ ] Time
+    - [x] Time
       - O(1) to add/remove at end (amortized for allocations for more space), index, or update
       - O(n) to insert/remove elsewhere
-    - [ ] Space
+    - [x] Space
       - contiguous in memory, so proximity helps performance
       - space needed = (array capacity, which is >= n) * size of item, but even if 2n, still O(n)
     - [Practice problems on Arrays](https://practice.geeksforgeeks.org/topics/Array/)
