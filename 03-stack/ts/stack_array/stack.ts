13190488699bb95634b672dd80587502ef140841
Blue Eu <eu.blue@pm.me>
2022-05-31 05:20:52 -0500

    TypeScript Stack added

---
+++ 03-stack/ts/stack_array/stack.ts
@@ -0,0 +1,50 @@
+class Stack<T> {
+    count: number;
+    storage: Array<T>;
+
+    constructor() {
+        this.count = 0;
+        this.storage = [];
+    }
+
+    push = (val: T) => {
+        this.storage[this.count] = val;
+        this.count++;
+    }
+
+    pop = () => {
+        if (this.count == 0) {
+            return undefined;
+        }
+
+        this.count--;
+        let val = this.storage[this.count];
+        delete this.storage[this.count];
+        return val;
+    }
+
+    size = () => {
+        return this.count;
+    }
+
+    peek = () => {
+        return this.storage[this.count - 1];
+    }
+
+    isEmpty = () => {
+        return this.count == 0;
+    }
+}
+
+const mystack = new Stack<number>();
+mystack.push(1);
+mystack.push(2);
+console.log(mystack.isEmpty());
+console.log(mystack.peek());
+console.log(mystack.pop());
+console.log(mystack.pop());
+console.log(mystack.size());
+console.log(mystack.peek());
+console.log(mystack.pop());
+console.log(mystack.peek());
+console.log(mystack.isEmpty());
