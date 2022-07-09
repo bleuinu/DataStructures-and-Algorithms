09b5ad6afe9db0bc519513ce027859792684da92
Blue Eu <eu.blue@pm.me>
2022-07-09 09:51:36 +0900

    ruby - stack linked list code modified

--- 03-stack/stack-linkedlist/ruby/main.rb
+++ 03-stack/stack-linkedlist/ruby/main.rb
@@ -10,16 +10,16 @@ end
 
 class Stack 
     def initialize 
-        @head = nil
+        @top = nil
     end 
 
     def push(data)
-        if @head == nil
-            @head = Node.new(data)
+        if self.empty?
+            @top = Node.new(data)
         else
             temp = Node.new(data)
-            temp.next = @head
-            @head = temp
+            temp.next = @top
+            @top = temp
         end
     end 
 
@@ -28,9 +28,9 @@ class Stack
             puts "stack is empty..."
             return
         else
-            data = @head.data
-            temp = @head
-            @head = @head.next
+            data = @top.data
+            temp = @top
+            @top = @top.next
             temp = nil
             return data
         end
@@ -40,12 +40,12 @@ class Stack
         if self.empty?
             puts "stack is empty..."
         else 
-            return @head.data
+            return @top.data
         end 
     end 
 
     def empty?
-        @head == nil
+        @top == nil
     end 
 
     def to_s
@@ -54,7 +54,7 @@ class Stack
             return
         end 
 
-        temp = @head
+        temp = @top
 
         str = ""
         while temp != nil 
